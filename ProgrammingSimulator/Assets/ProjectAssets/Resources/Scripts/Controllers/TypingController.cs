b6b9ea671fab59975cc3d5b188a80c0d91ffbcbb
StasVersh <71280619+StasVersh@users.noreply.github.com>
2022-04-21 19:54:14 +0300

    Create SDos scene

--- ProgrammingSimulator/Assets/ProjectAssets/Resources/Scripts/Controllers/TypingController.cs
+++ ProgrammingSimulator/Assets/ProjectAssets/Resources/Scripts/Controllers/TypingController.cs
@@ -1,13 +1,17 @@
+using System.Collections.Generic;
 using System.IO;
 using ProjectAssets.Pages.Notepad.Scripts.Models;
 using ProjectAssets.Resources.Scripts.Models;
 using TMPro;
+using TMPro.Examples;
 using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
 
 public class TypingController : MonoBehaviour
 {
+    [SerializeField] private List<string> _codeList;
+
     private TMP_Text _textForWriting;
     private ScrollRect _scrollRect;
     private Player _player;
@@ -18,7 +22,7 @@ public class TypingController : MonoBehaviour
     private void Construct(Code code, Player player)
     {
         _textForWriting = code.Text;
-        _textForWriting.text = File.ReadAllText("C:\\Users\\micro\\Documents\\GitHub\\ps\\ProgrammingSimulator\\Assets\\ProjectAssets\\Resources\\Code\\C#\\01.txt");
+        _textForWriting.text = File.ReadAllText(_codeList[Random.Range(0, _codeList.Count - 1)]);
         _codeBody = code.Text.text;
         _player = player;
         _scrollRect = code.ScrollRect;
@@ -46,7 +50,7 @@ public class TypingController : MonoBehaviour
             }
             else
             {
-                _codeBody += File.ReadAllText($"C:\\Users\\micro\\Documents\\GitHub\\ps\\ProgrammingSimulator\\Assets\\ProjectAssets\\Resources\\Code\\C#\\0{(int)(Random.Range(1,8))}.txt");
+                _codeBody += File.ReadAllText(_codeList[Random.Range(0, _codeList.Count - 1)]);;
             }
         }
     }
---
+++ ProgrammingSimulator/Assets/ProjectAssets/Resources/Scripts/Models/TabModel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace ProjectAssets.Resources.Scripts.Models
+{
+    public class TabModel
+    {
+        public Button Button { get; }
+        public int Index { get; }
+        public GameObject Page { get; }
+
+        public TabModel(Button button, int index, GameObject page)
+        {
+            Button = button;
+            Index = index;
+            Page = page;
+        }
+    }
+}
